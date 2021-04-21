20ecc543f3129df69249a425643444728679c669
CameliaZalum <61459440+CameliaZalum@users.noreply.github.com>
2021-04-21 11:12:31 +0300

    comments on Hamilton graph

--- lab6/src/Hamilton.java
+++ lab6/src/Hamilton.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Creates an Runnable entity that finds a Hamilton Cycle in a graph
+ */
 public class Hamilton implements Runnable {
 
     private Graph graph;
@@ -8,6 +10,12 @@ public class Hamilton implements Runnable {
     private List<Integer> path;
     private List<Integer> result;
 
+    /**
+     * Constructor that initiates the parameters for the graph
+     * @param graph
+     * @param node the start node for seraching for a cycle
+     * @param result the final path of the cycle
+     */
     Hamilton(Graph graph, int node, List<Integer> result) {
         this.graph = graph;
         this.startingNode = node;
@@ -15,29 +23,39 @@ public class Hamilton implements Runnable {
         this.result = result;
     }
 
+    /**
+     * Starts the search
+     */
     @Override
     public void run() {
         visit(startingNode);
     }
 
+    /**
+     * Sets the path for the resulting cycle
+     */
     private void setResult() {
         this.result.clear();
         this.result.addAll(this.path);
     }
 
+    /**
+     * Tahes as parameter a node and checkes if there exist a 
+     * cycle that contains that node in its path as starting node
+     */
     private void visit(int node) {
-        path.add(node);
+        path.add(node); // adds the node to the path
 
-        if (path.size() == graph.size()) {
-            if (graph.getNeighbours(node).contains(startingNode)) {
-                setResult();
+        if (path.size() == graph.size()) { // checks if the obtained path is a cycle
+            if (graph.getNeighbours(node).contains(startingNode)) { // checks if there exists an edge between the last node and the starting node to mark the existance of a cycle
+                setResult(); // marks a found result
             }
             return;
         }
 
-        for (int neighbour : graph.getNeighbours(node)) {
-            if (!this.path.contains(neighbour)) {
-                visit(neighbour);
+        for (int neighbour : graph.getNeighbours(node)) { // takes the next neighbours og the current node
+            if (!this.path.contains(neighbour)) { // checks if the node is not already contained in the path
+                visit(neighbour); // parforms the next step for the current node
             }
         }
     }
